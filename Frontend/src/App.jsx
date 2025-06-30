e489fa9a25bff929da6b79c514113462e7c04350
Aryan Joshi <aryajnoshi033@gmail.com>
2025-06-30 22:22:58 +0530

    Pub/Sub phase started

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,6 +11,12 @@ function App() {
   const [login, setLogin] = useState(false);
   const [onlineUsers, setOnlineUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState();
+  const [message, setMessage] = useState("");
+  const [chatId, setChatId] = useState("");
+  const [messages, setMessages] = useState([]);
+  // testing
+  // const [senderId, setSenderId] = useState();
+  // const [sender, setSender] = useState();
 
   useEffect(() => {
     if (!token) {
@@ -23,26 +29,28 @@ function App() {
     });
 
     newSocket.on("connect", () => {
-      console.log("Connected to server");
-      console.log("Socket connected with ID:", newSocket.id);
+      // console.log("Connected to server");
+      // console.log("Socket connected with ID:", newSocket.id);
 
       getOnlineUsers();
     });
 
     newSocket.on("user-online", ({ userName }) => {
-      console.log(`${userName} came online`);
-      console.log("Socket connected with ID:", newSocket.id);
+      // console.log(`${userName} came online`);
+      // console.log("Socket connected with ID:", newSocket.id);
     });
 
     newSocket.on("user-offline", ({ userName }) => {
-      console.log(`${userName} went offline`);
+      // console.log(`${userName} went offline`);
     });
+
+    // })
 
     setSocket(newSocket);
 
     return () => {
       newSocket.disconnect();
-      console.log("disconnected");
+      // console.log("disconnected");
       getOnlineUsers();
     };
   }, [token]);
@@ -53,7 +61,62 @@ function App() {
       setToken(storedToken);
       setLogin(true);
     }
+    const savedUser = localStorage.getItem("selectedUser");
+    if (savedUser) {
+      setSelectedUser(JSON.parse(savedUser));
+    }
   }, []);
+
+  // not good i guess
+  useEffect(() => {
+    if (!socket) return;
+
+    // if(selectedUser){
+
+    // }
+
+    const handleMessage = (msg) => {
+      // console.log("New message received", msg);
+      setMessages((prev) =>
+        [...prev, msg].sort(
+          (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+        )
+      );
+    };
+
+    console.log(messages);
+
+    socket.on("new_message", handleMessage);
+
+    return () => socket.off("new_message", handleMessage);
+  }, [socket]);
+
+  useEffect(() => {
+    if (selectedUser) {
+      localStorage.setItem("selectedUser", JSON.stringify(selectedUser));
+    }
+  }, [selectedUser]);
+
+  useEffect(() => {
+    if (selectedUser && token && socket) {
+      handleSelectedUser(selectedUser);
+    }
+  }, [selectedUser, token, socket]);
+
+  // useEffect(() => {
+  //   if (!socket) return;
+
+  //   // const mesres = axios.get(
+  //   //   `http://localhost:5000/api/user/getMessages?recipientId=${recipientId}`,
+
+  //   //   {
+  //   //     headers: {
+  //   //       Authorization: `Bearer ${token}`,
+  //   //     },
+  //   //   }
+  //   // );
+  //   setMessages(mesres.data.messages);
+  // }, [messages]);
 
   async function getOnlineUsers() {
     const data = await axios.get(
@@ -68,6 +131,7 @@ function App() {
     console.log(data);
 
     setOnlineUsers(data.data);
+    console.log(onlineUsers);
   }
 
   const handleSubmit = async (e) => {
@@ -111,19 +175,67 @@ function App() {
       setLogin(false);
       setSocket(null);
 
-      console.log("Logout successful");
+      // console.log("Logout successful");
     } catch (err) {
       console.error("Logout error:", err.message);
     }
   };
 
-  const handleSelectedUser = async (user) => {
-    console.log(user.username);
-    setSelectedUser(user);
+  const getUserName = (id) => {
+    //console.log(messages);
+    const user = onlineUsers.find((user) => user._id === id);
+    // console.log(user);
+    return user ? user.username : "You";
   };
 
-  const handleMessage = async () => {
-    alert("message alert");
+  const handleSelectedUser = async (user) => {
+    const recipientId = user._id;
+
+    const res = await axios.post(
+      "http://localhost:5000/api/user/access-chat-or-create",
+      { recipientId },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const mesres = await axios.get(
+      `http://localhost:5000/api/user/getMessages?recipientId=${recipientId}`,
+
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    console.log(res);
+    console.log(mesres);
+    const chatId = res.data.chatId;
+    setChatId(chatId);
+    setMessages(
+      [...mesres.data.messages].sort(
+        (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+      )
+    );
+    // console.log("Chat ready with ID:", chatId);
+  };
+
+  const sendMessage = async () => {
+    if (!chatId || !message.trim() || !socket) {
+      alert("Missing chat ID or message.");
+      return;
+    }
+
+    socket.emit("send_message", {
+      chatId,
+      content: message,
+      type: "text",
+    });
+
+    setMessage("");
   };
   return (
     <div className="app-container">
@@ -131,7 +243,7 @@ function App() {
         <div className="logged-in">
           <ul>
             {onlineUsers.map((user, index) => (
-              <li key={user._id} onClick={() => handleSelectedUser(user)}>
+              <li key={user._id} onClick={() => setSelectedUser(user)}>
                 {user.username} - {user.status}
               </li>
             ))}
@@ -139,13 +251,25 @@ function App() {
           {selectedUser ? (
             <div className="Selected-User">
               <div className="Selected-User-Username">
-                {selectedUser.userName}
+                {selectedUser.username}
               </div>
+              <div className="Messages">
+                {messages.map((msg, i) => (
+                  <div key={i}>
+                    <strong>{getUserName(msg.sender._id)}:</strong>{" "}
+                    {msg.content}
+                  </div>
+                ))}
+                {/* {messages} */}
+              </div>
+
               <div className="Selected-User-MessageBox">
-                <div>
-                  <input type="text" />
-                  <button onClick={handleMessage}>Send</button>
-                </div>
+                <input
+                  type="text"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+                <button onClick={sendMessage}>Send</button>
               </div>
             </div>
           ) : (
