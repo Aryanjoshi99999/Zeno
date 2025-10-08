981c53c24e7f94b4dffc586ebac9d789c193301d
Aryan Joshi <aryajnoshi033@gmail.com>
2025-10-08 18:17:04 +0530

    bug fixed

--- Frontend/src/apiClient.js
+++ Frontend/src/apiClient.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
-const backendUrl = "http://localhost:5000";
+const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 
 const apiClient = axios.create({
   baseURL: backendUrl,
